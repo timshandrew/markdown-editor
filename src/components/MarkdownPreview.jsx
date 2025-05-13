edec50c213bd695c31d45a5e922f463fdc7bdc04
Andrew <andrew.alan.sm@gmail.com>
2025-05-13 16:10:23 +0100

    begin styling preview

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
 import { ContentContext } from "../contexts/ContentContext";
+import "../styles/previewStyles.css";
+import Markdown from "react-markdown";
 
 export default function MarkdownPreview() {
   const markdown = useContext(ContentContext);
 
   return (
-    <>
-      <p>{markdown}</p>
-    </>
+    <div className="markdown-preview">
+      <Markdown>{markdown}</Markdown>
+    </div>
   );
 }
