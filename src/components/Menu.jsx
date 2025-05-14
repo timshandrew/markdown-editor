65e888f49bc185a0899a9c8fb28bceea44a16338
Andrew <andrew.alan.sm@gmail.com>
2025-05-14 16:15:19 +0100

    style side menu outer layout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,18 @@
 import FileListItem from "./FileListItem";
+import logo from "../assets/logo.svg";
 
 export default function Menu({ visible }) {
-  const visibleClasses = visible ? "w-auto p-4" : "w-0 p-0";
+  const visibleClasses = visible ? "w-auto p-6" : "w-0 p-0";
 
   return (
     <nav
-      className={`bg-orange text-100 row-span-2 row-start-1 overflow-hidden py-4 ${visibleClasses}`}
+      className={`bg-900 text-100 row-span-2 row-start-1 flex flex-col items-start gap-6 overflow-hidden ${visibleClasses}`}
     >
-      <h1 className="w-max">Markdown</h1>
-      <h2 className="w-max">My Documents</h2>
-      <button className="w-max">+ New Document</button>
+      <img src={logo} alt="Company logo" />
+      <h2 className="text-500 text-heading-s w-max uppercase">My Documents</h2>
+      <button className="bg-orange text-heading-m w-max rounded-md px-12 py-3">
+        + New Document
+      </button>
       <ul className="w-max">
         <FileListItem creationDate="01-01-2025" fileName="fileA.md" />
       </ul>
