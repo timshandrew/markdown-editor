83b128b3b845db266b6546fe711a29bfe860887b
Andrew <andrew.alan.sm@gmail.com>
2025-05-22 14:39:01 +0100

    add labels for theme switch
    the sun and moon icons on the theme switch are now labels, increasing the clickable area of the theme switch.

--- src/components/ui/ThemeSwitch.jsx
+++ src/components/ui/ThemeSwitch.jsx
@@ -3,10 +3,20 @@ import { Switch } from "./switch";
 
 export default function ThemeSwitch({ theme, switchTheme, className }) {
   return (
-    <div className={`flex cursor-pointer gap-2 ${className}`}>
-      <LightModeIcon className={theme === "light-theme" && "fill-100"} />
-      <Switch className="cursor-pointer" onClick={switchTheme} />
-      <DarkModeIcon className={theme === "dark-theme" && "fill-100"} />
+    <div className={`flex gap-2 ${className}`}>
+      <label className="cursor-pointer" htmlFor="themeSwitch">
+        <LightModeIcon className={theme === "light-theme" && "fill-100"} />
+      </label>
+
+      <Switch
+        id="themeSwitch"
+        className="cursor-pointer"
+        onClick={switchTheme}
+      />
+
+      <label className="cursor-pointer" htmlFor="themeSwitch">
+        <DarkModeIcon className={theme === "dark-theme" && "fill-100"} />
+      </label>
     </div>
   );
 }
