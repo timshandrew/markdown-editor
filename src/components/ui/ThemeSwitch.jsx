ddbc8fff62ebb3f069f225c9f4e428cd4dd83202
Andrew <andrew.alan.sm@gmail.com>
2025-05-24 19:02:36 +0100

    fix theme switch
    position of light and dark mode icons now matches design.
    
    also now retains correct position between reloads to match theme setting

--- src/components/ui/ThemeSwitch.jsx
+++ src/components/ui/ThemeSwitch.jsx
@@ -5,17 +5,18 @@ export default function ThemeSwitch({ theme, switchTheme, className }) {
   return (
     <div className={`flex gap-2 ${className}`}>
       <label className="cursor-pointer" htmlFor="themeSwitch">
-        <LightModeIcon className={theme === "light" && "fill-100"} />
+        <DarkModeIcon className={theme === "dark" && "fill-100"} />
       </label>
 
       <Switch
         id="themeSwitch"
         className="cursor-pointer"
         onClick={switchTheme}
+        checked={theme === "light"}
       />
 
       <label className="cursor-pointer" htmlFor="themeSwitch">
-        <DarkModeIcon className={theme === "dark" && "fill-100"} />
+        <LightModeIcon className={theme === "light" && "fill-100"} />
       </label>
     </div>
   );
